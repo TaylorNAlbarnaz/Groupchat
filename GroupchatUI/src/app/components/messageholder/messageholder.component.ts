d3476cd8ff05d31815a6fec1fa33ed5c51a21d05
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-12-27 23:29:54 -0300

    Mensagens e Scroll

--- GroupchatUI/src/app/components/messageholder/messageholder.component.ts
+++ GroupchatUI/src/app/components/messageholder/messageholder.component.ts
@@ -2,8 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'message-holder',
-  templateUrl: './messageholder.component.html'
+  templateUrl: './messageholder.component.html',
+  styles: ['.holder {overflow-y: scroll; -ms-overflow-style: none; scrollbar-width: none;}',
+  '.holder::-webkit-scrollbar {display: none;}',
+  '.bt-shadow {background: linear-gradient(180deg, rgba(255,255,255,0) 0%, rgba(0,0,0,0.6) 100%); transition: 0.3s;}']
 })
 export class MessageHolderComponent {
+  shadowRef: Element | null;
+  atBottom: boolean = true;
 
+  ngAfterViewInit() {
+    this.shadowRef = document.querySelector('.holder');
+  }
+
+  onScroll() {
+    this.atBottom = (this.shadowRef?.scrollTop == 0);
+  }
+
+  resetScroll() {
+    if (this.shadowRef && !this.atBottom) {
+      this.shadowRef.scrollTop = 0;
+    }
+  }
 }
