c084f0671a8c8f0c81d7103a3cf4bc131f8e2fd4
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-01-13 11:21:39 -0300

    Removida criação de mensagens sem grupo

--- GroupchatAPI/GroupchatAPI/Controllers/MessagesController.cs
+++ GroupchatAPI/GroupchatAPI/Controllers/MessagesController.cs
@@ -28,34 +28,6 @@ namespace GroupchatAPI.Controllers
             return Ok(dbMessage);
         }
 
-        [HttpPost]
-        public async Task<ActionResult> CreateMessage(MessageDto messageDto)
-        {
-            var dbMessage = await context.Users.FindAsync(messageDto.Id);
-            if (dbMessage != null)
-                return BadRequest("This MessageId already exists!");
-
-            if (messageDto.Id < 0)
-                return BadRequest("Invalid Message Index!");
-
-            var dbUser = await context.Users.FindAsync(messageDto.UserId);
-            if (dbUser == null)
-                return NotFound("User not found!");
-
-            if (messageDto.Content == string.Empty)
-                return BadRequest("Message cannot be empty!");
-
-            var message = new Message {
-                User = dbUser,
-                Content = messageDto.Content
-            };
-
-            context.Messages.Add(message);
-            await context.SaveChangesAsync();
-
-            return Ok($"Message successfully created!"); ;
-        }
-
         [HttpPut]
         public async Task<ActionResult> UpdateMessage(MessageDto messageDto)
         {
